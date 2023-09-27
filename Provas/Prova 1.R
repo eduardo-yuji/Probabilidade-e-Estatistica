be51a397887ef164075e2817cf30ac6c7638454d
Eduardo Yuji Yoshida Yamada <a2320606@vmpdc.utfpr>
2023-09-27 09:06:00 -0300

    2709

--- Provas/Prova 1.R
+++ Provas/Prova 1.R
@@ -13,7 +13,14 @@ ggplot(dados,aes(x=resistencia, y=fabricante, fill=tamanho))+geom_boxplot()+coor
 #3
 interacao=interaction(dados$fabricante,dados$tamanho)
 dados1=as.data.frame(tapply(dados$resistencia, interacao, mean))
-d1$fabricante_tamanho=labels(d1)[[1]]
 names(dados1)='media'
+dados1$tamanho=labels(dados1)[[1]]
 head(dados1)
-ggplot(dados1,aes(x=media,y=))+geom_bar()
+ggplot(dados1,aes(x=media,y=tamanho,fill=media))+geom_col()+coord_flip()
+
+#4
+ggplot(dados,aes(tempo,peso,color=tamanho))+geom_point()+geom_smooth(se=F)
+
+
+#5
+ggplot(dados,aes(x=fabricante,y=resistencia,fill=fabricante))+geom_violin()+labs(title='Resistencia por tipo de fabricante',x='Fabricante',y='Resistencia')
